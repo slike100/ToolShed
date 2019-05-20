18de9397de30aa02a2ae6af1fa04765cd0546ae4
Sam Slike <samslike@Techtonics-MacBook-Pro.local>
2019-05-20 17:59:02 -0600

    fixed the add route with try catch

--- functions/controllers/toolController.js
+++ functions/controllers/toolController.js
@@ -14,24 +14,21 @@ toolController.get('/', (req, res) => {
 
 toolController.post('/newTool', (req, res) => {
   console.log('We are in the add tool route!');
-  console.log('database', db);
-  var data = {
-    name: 'Hammer',
-    description: 'This is the sickest hammer we have ever seen.',
-    isRented: false,
-    owner: 'UserID',
-    photo: "photo url string where it lives in cloud storage",
-    priceRatePerDay: 5,
-    rentalDurationInDays: 5
-  };
-  db.collection('Tools').doc().set(data).then(stuff => {
-    console.log(stuff, "this is the full document");
-    console.log(stuff.id, "this is the stuff ID");
-    return res.status(200).send(stuff);
-  }).catch(err => {
-    console.log(err);
-    return res.status(500).send(err);
-  });
+  try {
+    db.collection('Tools').add({
+      name: 'Saw',
+      description: 'This is the sickest Sawwwww we have ever seen.',
+      isRented: false,
+      owner: 'UserID',
+      photo: "photo url string where it lives in cloud storage",
+      priceRatePerDay: 6,
+      rentalDurationInDays: 100
+    }).then(() => {
+      return res.status(200).send('we are in the confirm, we added a tool');
+    });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
 
 module.exports = toolController;
