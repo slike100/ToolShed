1b10268d5fa966ddc511d0f3e396c6e8dfbbbf3b
Sam Slike <samslike@Techtonics-MacBook-Pro.local>
2019-05-20 16:01:18 -0600

    initial firebase setup complete

---
+++ functions/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const firebase = require('firebase');
+// const admin = require('firebase-admin');
+
+var config = {
+  apiKey: 'AIzaSyBoBebgi0tvoGb2sPRP4C0y97n3Kgk5fNc',
+  projectId: 'toolshed-1dd98'
+};
+
+firebase.initializeApp(config);
