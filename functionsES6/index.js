188f97c69268082696596e0c78b3ba7c7138a2ac
Daniel Scanland <daniel.scanland@techtonic.com>
2019-05-21 09:29:39 -0600

    added userController

--- functionsES6/index.js
+++ functionsES6/index.js
@@ -1,6 +1,11 @@
-const functions = require('firebase-functions');
-const toolController = require('./controllers/toolController') 
+const functions = require("firebase-functions");
+const toolController = require("./controllers/toolController");
+const userController = require("./controllers/userController");
 
 exports.tool = functions.https.onRequest((req, res) => {
-  return toolController(req,res)
+  return toolController(req, res);
+});
+
+exports.user = functions.https.onRequest((req, res) => {
+  return userController(req, res);
 });
