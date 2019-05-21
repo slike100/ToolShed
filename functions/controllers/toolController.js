ef3752a471aa44f31be7d0702906c99ba6b45bfe
Daniel Scanland <daniel.scanland@techtonic.com>
2019-05-21 10:13:35 -0600

    post newTool endpoint

--- functions/controllers/toolController.js
+++ functions/controllers/toolController.js
@@ -14,15 +14,16 @@ toolController.get('/', (req, res) => {
 
 toolController.post('/newTool', (req, res) => {
   console.log('We are in the add tool route!');
+  console.log('this is req.body', req.body);
   try {
     db.collection('Tools').add({
-      name: 'Saw',
-      description: 'This is the sickest Sawwwww we have ever seen.',
-      isRented: false,
-      owner: 'UserID',
-      photo: "photo url string where it lives in cloud storage",
-      priceRatePerDay: 6,
-      rentalDurationInDays: 100
+      name: req.body.name,
+      description: req.body.description,
+      isRented: req.body.isRented,
+      owner: req.body.owner,
+      photo: req.body.photo,
+      priceRatePerDay: req.body.priceRatePerDay,
+      rentalDurationInDays: req.body.rentalDurationInDays
     }).then(() => {
       return res.status(200).send('we are in the confirm, we added a tool');
     });
@@ -30,5 +31,23 @@ toolController.post('/newTool', (req, res) => {
     return res.status(500).send(error);
   }
 });
+
+//START DELETE TOOL ENDPOINT//
+
+
+//START DELETE TOOL ENDPOINT//
+
+
+//START UPDATE TOOL ENDPOINT//
+
+
+//END UPDATE TOOL ENDPOINT//
+
+
+//START GET TOOLS BY LAT & LONG AND OPTIONALLY BY NAME OF TOOL ENDPOINT//
+
+
+//START GET TOOLS BY LAT & LONG AND OPTIONALLY BY NAME OF TOOL ENDPOINT//
+
 
 module.exports = toolController;
--- functions/controllers/userController.js
+++ functions/controllers/userController.js
@@ -12,4 +12,45 @@ userController.get("/", (req, res) => {
   res.send("sup");
 });
 
+//START NEW USER POST ENDPOINT//
+userController.post('/newUser', (req, res) => {
+  console.log('We are in the add new user route!');
+  console.log('this is req.body', req.body);
+  try {
+    db.collection('User').add({
+      userName: req.body.userName,
+      location: req.body.location,
+      avatar: req.body.avatar,
+      toolsOwned: [],
+      toolsBeingRented: [],
+      token: req.body.token
+    }).then(() => {
+      return res.status(200).send('we are in the confirm, added new user');
+    });
+  } catch (error) {
+    return res.status(500).send('could not add new user', error);
+  }
+});
+
+//END NEW USER POST ENDPOINT//
+
+
+//START DELETE USER ENPOINT//
+
+
+//END DELETE USER ENDPOINT//
+
+
+//START UPDATE USER ENDPOINT//
+
+
+//SEND UPDATE USER ENDPOINT//
+
+
+//START GET ONE USER BY USERID//
+
+
+//END GET ONE USER BY USERID//
+
+
 module.exports = userController;
