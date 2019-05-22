a6acabb5ec22c47efd0847c1939e26dc32a1931f
David Brown <davidbrown@Techtonics-MacBook-Pro.local>
2019-05-22 09:37:41 -0600

    a few more changes, finished removing test action and reducer

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -5,14 +5,12 @@ import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import rootReducer from "./reducers/index";
 
 export default (function configureStore() {
-    const composeEnhancers =
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
 
-    // return our store, passing in our root reducer, our initial state, our middleware
-    return createStore(
-        rootReducer,
-        composeEnhancers(applyMiddleware(thunk,
-            reduxImmutableStateInvariant()
-        ))
-    );
+  // return our store, passing in our root reducer, our initial state, our middleware
+  return createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
 })();
