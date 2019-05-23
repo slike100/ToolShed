508bab639bf4881b97b7c4bbb836fabf707d57d7
Daniel Scanland <daniel.scanland@techtonic.com>
2019-05-23 10:43:12 -0600

    added user reducers

--- front-end/src/redux/reducers/userReducer.js
+++ front-end/src/redux/reducers/userReducer.js
@@ -1,13 +1,30 @@
-import //Import TYPES here
-"../types/userTypes";
+import {
+  NEW_USER,
+  DELETE_USER,
+  EDIT_USER,
+  GET_USER_DATA,
+} from "../types/userTypes";
 
 const initialState = {
-  //Specify initial tool state here if needed
+  auth: null,
+  user: null,
 };
 
 export default function userReducer(state = initialState, action) {
-  switch (action.type) {
-    //add switch cases here
+  const { type, payload } = action;
+
+  switch (type) {
+    case NEW_USER:
+      return { ...state, auth: payload, user: payload };
+
+    case DELETE_USER:
+      return { ...state, auth: null };
+
+    case EDIT_USER:
+      return { ...state, auth: payload, user: payload };
+
+    case GET_USER_DATA:
+      return { ...state, auth: payload, user: payload };
 
     default:
       return state;
