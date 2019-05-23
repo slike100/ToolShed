7f2ca65a17b8a16b2b749012c79ddacba1ff4a04
Nathaniel Wendt <nathaniel.wendt@techtonic.com>
2019-05-23 14:19:10 -0600

    fix: remove unnecessary additional login (facebook)

--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -1,62 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import firebase, { auth, provider } from '../utils/firebaseConfig';
+
+import Navbar from "./Navbar";
 
 // import actions here if needed
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    };
-  }
-
-  // handleChange = (e) => {
-
-  // }
-
-  login = () => {
-    auth.signInWithPopup(provider)
-      .then((result) => {
-        const user = result.user;
-        this.setState({
-          user
-        });
-      });
-  }
-
-  logout = () => {
-    auth.signOut()
-      .then(() => {
-        this.setState({
-          user: null,
-        });
-      });
-  }
-
-  componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user });
-      }
-    });
-  }
 
   render() {
     // console.log(this.props);
     return (
       <div>
-        <header>
-          <div className="wrapper">
-            <h1>ToolShed</h1>
-            {this.state.user ?
-              <button onClick={this.logout}>Logout</button>
-              :
-              <button onClick={this.login}>Login</button>
-            }
-          </div>
-        </header>
+        <Navbar />
       </div>
     )
   }
