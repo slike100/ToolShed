25c9f4d0ced4a4789e016e4e536667fc7624bb00
Samantha Morgan <samantha.morgan@techtonic.com>
2019-05-29 10:56:58 -0600

    turned this component into a modal and fixed the styling

--- front-end/src/components/Checkout.js
+++ front-end/src/components/Checkout.js
@@ -1,68 +1,74 @@
 import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
+import options from "materialize-css/dist/js/materialize.min.js";
 import React from "react";
 import { connect } from "react-redux";
-import { Elements, StripeProvider } from 'react-stripe-elements';
+import { Elements, StripeProvider } from "react-stripe-elements";
 import CheckoutForm from "./stripe";
 
-import './CSS/Checkout.css';
+import "./CSS/Checkout.css";
 
 class Checkout extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: null,
+      user: null
     };
   }
 
-
-
+  componentDidMount() {
+    const elems = document.querySelectorAll(".modal");
+    const instances = M.Modal.init(elems, options);
+  }
 
   render() {
     return (
+      <div id="checkoutModal" class="modal">
+        <div class="modal-content">
+          <div className="checkoutForm">
+            <form className="borderRadius">
+              <h3>Rental Details</h3>
 
-      <div className="checkoutForm">
+              <label for="toolModel">Pick-up Location:</label>
+              <h5>Denver, CO</h5>
 
-        <form className="borderRadius" >
-          <h3>Rental Details</h3>
+              <label for="toolModel">Rental Duration:</label>
+              <h5>3 Days</h5>
 
-          <label for="toolModel">Pick-up Location:</label>
-          <h5>Denver, CO</h5>
+              <label for="toolModel">Total:</label>
+              <h5>$45</h5>
 
-          <label for="toolModel">Rental Duration:</label>
-          <h5>3 Days</h5>
-
-
-          <label for="toolModel">Total:</label>
-          <h5>$45</h5>
-
-
-          <div className="">
-            <StripeProvider apiKey="pk_test_MOtKUdvLk0HzhkWZ5l8gtg6j00j5CMoeeI">
-              <div className="example">
-                <Elements>
-                  <CheckoutForm />
-                </Elements>
+              <div className="">
+                <StripeProvider apiKey="pk_test_MOtKUdvLk0HzhkWZ5l8gtg6j00j5CMoeeI">
+                  <div className="example">
+                    <Elements>
+                      <CheckoutForm />
+                    </Elements>
+                  </div>
+                </StripeProvider>
               </div>
-            </StripeProvider>
+            </form>
           </div>
-
-        </form>
+        </div>
       </div>
-
-
-
-
-    )
+    );
   }
-
 }
 
 export default Checkout;
 
+//this is the button functionality for whichever page will render this modal
+//be sure to put the component itself inside the render of the page that is rendering this
+//  <button
+// class="btn-large waves-effect waves-light btn modal-trigger"
+// data-target="checkoutModal"
+// >
+// checkout
+// </button>
 
-
-{/* <div className="card checkoutCard">
+/* {
+  <div className="card checkoutCard">
 <div className="card-image">
   <img src="https://images.unsplash.com/photo-1557240231-9378fcdeefa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
 </div>
@@ -81,4 +87,5 @@ export default Checkout;
 </button>
 
 </div>
-</div> */}
+</div> 
+}*/
