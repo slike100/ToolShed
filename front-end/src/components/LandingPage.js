024566c685b8ebbf0e3f114fd593581a26879c20
Samantha Morgan <samantha.morgan@techtonic.com>
2019-05-29 12:16:52 -0600

    completed jumbotron styling

--- front-end/src/components/LandingPage.js
+++ front-end/src/components/LandingPage.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-
-import '../components/CSS/LandingPage.css';
+import "../components/CSS/LandingPage.css";
 import "materialize-css/dist/css/materialize.min.css";
 // COMPONENT
-import Navbar from './Navbar';
-import About from './About';
-import Jumbotron from './Jumbotron';
+import Navbar from "./Navbar";
+import About from "./About";
+import Jumbotron from "./Jumbotron";
 //
 
 class LandingPage extends Component {
-
-    render(){
-        return(
-            <div>
-                {/* <Navbar />  */}
-                <Jumbotron />
-                <About />
-            </div> 
-        )
-    }
+  render() {
+    return (
+      <div>
+        {/* <Navbar />  */}
+        <Jumbotron />
+        <About />
+      </div>
+    );
+  }
 }
 export default LandingPage;
