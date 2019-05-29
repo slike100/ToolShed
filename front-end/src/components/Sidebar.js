dbf4b7fa7264c058b43416f027115699f0006ff1
Nathaniel Wendt <nathaniel.wendt@techtonic.com>
2019-05-28 19:14:46 -0600

    WIP: create form for tool and address with general styling

--- front-end/src/components/Sidebar.js
+++ front-end/src/components/Sidebar.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
-import { TextInput } from "react-materialize";
+import React from 'react';
 
-class Sidebar extends Component {
+class Sidebar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: '',
-      lng: '',
+      searchTool: '',
+      searchAddress: '',
+      searchResults: [],
     };
   }
 
-  handleSubmit = (e) => {
-    alert('The value is: ' + this.input.value);
+  handleChange = e => {
     e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert(this.state.searchTool + this.state.searchAddress);
   }
 
   render() {
     return (
-      <div id="sidebar">
-        <form id="search-form" onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-          <input type="text" ref={(input) => this.input = input} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+      <form id="sidebar" onSubmit={this.handleSubmit}>
+        <label class="sidebar-search-label">
+          What Tool do you need?
+          <input class="sidebar-search-input" type="text" value={this.state.searchTool} name="searchTool" onChange={this.handleChange} />
+        </label>
+        <label class="sidebar-search-label">
+          What is your full physical address?
+          <input class="sidebar-search-input" type="text" value={this.state.searchAddress} name="searchAddress" onChange={this.handleChange} />
+        </label>
+        <input class="sidebar-search-btn" type="submit" value="Search" />
+      </form>
     );
   }
 }
