ad0baa59ba32894d36e2df00d25c47d744755cda
Daniel Scanland <daniel.scanland@techtonic.com>
2019-06-03 10:01:58 -0600

    fixed jumbo

--- front-end/src/components/Jumbotron.js
+++ front-end/src/components/Jumbotron.js
@@ -12,14 +12,19 @@ class Jumbotron extends Component {
   render() {
     return (
       <div className="background">
-        
+        <div className="frontBanner">
+          <h1 className="frontSlogan">Never Buy Another Tool</h1>
+          <h4 className="underSlogan">A new way rent tools in your community</h4>
+
+        </div>
+
         {/* <img src={image} className="jumbo" /> */}
-        <SearchLandingPage/>
+        <SearchLandingPage />
         {/* <p className="baseline-text-bg">Way better than buying tools</p>
         <p class="baseline-text-bg">
           Rent thousands of tools from people all over your neighborhood
         </p> */}
-        
+
       </div>
 
       // {/* <img src={image} className="responsive-img"/> */}
